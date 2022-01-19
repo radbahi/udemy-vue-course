4f2e9de093d04cd9226f00a0c3dc20de715b6cc9
Radouane Bahi <radbahi@gmail.com>
2022-01-19 16:28:25 -0500

    finished section 9

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  // props: ['topics'],
+  //inject is how we take data from a provided parent/ancestor component
+  inject: ['topics'],
+};
+</script>
