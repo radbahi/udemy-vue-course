06bdcd2fc87427065e24070d10a5293067593d41
Radouane Bahi <radbahi@gmail.com>
2022-01-24 09:35:41 -0500

    section 11 done

---
+++ forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,71 @@
+<template>
+  <ul>
+    <!-- giving buttons a type=button overrides their default of being a submit -->
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  //   INSTEAD OF USING DATA OR COMPUTED TO READ MODELVALUE, WE SET THE CSS CLASS IN THE LIST ITEMS TO DO VISUAL FEEDBACK ON SETTING TO NULL
+  //   data() {
+  //     return {
+  //       activeOption: this.modelValue,
+  //     };
+  //   },
+  //   computed: {
+  //     activeOption() {
+  //       return this.modelValue;
+  //     },
+  //   },
+  methods: {
+    activate(option) {
+      this.activeOption = option;
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: purple;
+}
+
+.active button {
+  color: purple;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5 rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
